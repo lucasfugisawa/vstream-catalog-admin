ba18eba8413027e774288b81f028fb39898940a2
Lucas Fugisawa <lucasfugisawa@gmail.com>
2024-02-04 14:39:05 -0300

    Either refactor.

--- application/src/main/kotlin/com/fugisawa/vstream/catalog/admin/application/utils/Either.kt
+++ application/src/main/kotlin/com/fugisawa/vstream/catalog/admin/application/utils/Either.kt
@@ -6,16 +6,16 @@ sealed class Either<L, R> {
     data class Right<L, R>(override val value: R) : Either<L, R>()
 
     abstract val value: Any?
-    val hasLeft: Boolean = this is Left<L, R>
-    val hasRight: Boolean = this is Right<L, R>
-    fun leftOrNull(): L? = if (this is Left<L, R>) value else null
-    fun rightOrNull(): R? = if (this is Right<L, R>) value else null
-    fun leftOrThrow(throwable: Throwable): L = if (this is Left<L, R>) value else throw throwable
-    fun rightOrThrow(throwable: Throwable): R = if (this is Right<L, R>) value else throw throwable
-    fun leftOrDefault(defaultValue: L?): L? = if (this is Left<L, R>) value else defaultValue
-    fun rightOrDefault(defaultValue: R?): R? = if (this is Right<L, R>) value else defaultValue
-    fun leftOrElse(onRight: (right: R) -> L?): L? = if (this is Left<L, R>) value else onRight(value as R)
-    fun rightOrElse(onLeft: (left: L) -> R?): R? = if (this is Right<L, R>) value else onLeft(value as L)
+    val isLeft: Boolean = this is Left<L, R>
+    val isRight: Boolean = this is Right<L, R>
+    fun leftOrNull(): L? = if (isLeft) value as L else null
+    fun rightOrNull(): R? = if (isRight) value as R else null
+    fun leftOrThrow(throwable: Throwable): L = if (isLeft) value as L else throw throwable
+    fun rightOrThrow(throwable: Throwable): R = if (isRight) value as R else throw throwable
+    fun leftOrDefault(defaultValue: L?): L? = if (isLeft) value as L else defaultValue
+    fun rightOrDefault(defaultValue: R?): R? = if (isRight) value as R else defaultValue
+    fun leftOrElse(onRight: (right: R) -> L?): L? = if (isLeft) value as L else onRight(value as R)
+    fun rightOrElse(onLeft: (left: L) -> R?): R? = if (isRight) value as R else onLeft(value as L)
 
     fun <T> fold(fnL: (L) -> T, fnR: (R) -> T): T = when (this) {
         is Left -> fnL(value)
