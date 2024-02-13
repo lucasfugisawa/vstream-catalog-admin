6641b1f05e235cd659634236adde94878a8a8f29
Lucas Fugisawa <lucasfugisawa@gmail.com>
2024-02-12 23:07:50 -0300

    Integrating Spring Boot to the infrastructure module.

--- infrastructure/src/main/kotlin/com/fugisawa/vstream/catalog/admin/infrastructure/Main.kt
+++ infrastructure/src/main/kotlin/com/fugisawa/vstream/catalog/admin/infrastructure/Main.kt
@@ -1,6 +1,11 @@
 package com.fugisawa.vstream.catalog.admin.infrastructure
 
-fun main(args: Array<String>) {
-    println("Hello World: " + args)
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
+@SpringBootApplication
+class Main
+
+fun main(args: Array<String>) {
+    runApplication<Main>(*args)
 }
