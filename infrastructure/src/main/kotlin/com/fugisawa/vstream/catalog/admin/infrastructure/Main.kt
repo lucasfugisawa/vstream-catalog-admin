d1045a583e4191c9439082f50b010a6222d969aa
Lucas Fugisawa <lucasfugisawa@gmail.com>
2024-02-12 23:21:06 -0300

    Server / Undertow and default profile configurations.

--- infrastructure/src/main/kotlin/com/fugisawa/vstream/catalog/admin/infrastructure/Main.kt
+++ infrastructure/src/main/kotlin/com/fugisawa/vstream/catalog/admin/infrastructure/Main.kt
@@ -2,10 +2,12 @@ package com.fugisawa.vstream.catalog.admin.infrastructure
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.core.env.AbstractEnvironment
 
 @SpringBootApplication
 class Main
 
 fun main(args: Array<String>) {
+    System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development")
     runApplication<Main>(*args)
 }
